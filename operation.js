09ada4f5bcaa8161e3d68827cea4fa8fb75da438
1007Anna <anna78811146@gmail.com>
2023-04-20 15:02:38 +0800

    02

--- operation.js
+++ operation.js
@@ -3,15 +3,27 @@ const input02 = document.getElementById("input02");
 const result = document.getElementById("result");
 const btn = document.getElementById("button");
 const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const times = document.getElementById("times");
+const dividedBy = document.getElementById("dividedBy");
 const select = document.getElementById("select");
 
 btn.addEventListener("click",function(){
     const num01 = +input01.value;
     const num02 = +input02.value;
-    const sum = num01 + num02;
+    const addSum = num01 + num02;
+    
+    // const minusSum = num01 - num02;
+    // const timesSum = num01 * num02;
+    // const dividedBySum = num01 / num02;
+
+
     // input01.value = null;
     // input02.value = null;
-    result.innerText = sum;
+    result.innerText = "計算結果：" + addSum;
+    // result.innerText = "計算結果：" + minusSum;
+    // result.innerText = "計算結果：" + timesSum;
+    // result.innerText = "計算結果：" + dividedBySum;
     
 
     // console.log(typeof num01)
